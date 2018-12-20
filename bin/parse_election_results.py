ed2ddb26d61ce691d89b000340d2a948d102632a
Breyten Ernsting <bje@dds.nl>
2018-12-20 16:52:03 +0100

    Use google for search.

--- bin/parse_election_results.py
+++ bin/parse_election_results.py
@@ -4,7 +4,10 @@ import sys
 import os
 import re
 import csv
-import codecs
+from time import sleep
+import json
+
+from googlesearch import search
 
 
 def import_results():
@@ -29,12 +32,41 @@ def import_parties():
             result += row
     return list(set([r.lower() for r in result]))
 
+
+def get_local_parties_results(results, parties):
+    local_parties = []
+    for result in results:
+        if re.sub('\s*\(.+\)$', '', result['Partij'].lower()) not in parties:
+            local_parties.append(result)
+    return local_parties
+
+
+def get_local_party_links(local_party):
+    search_results = search(
+        '%s %s' % (local_party['Partij'].lower(), local_party['RegioNaam'].lower(),), stop=20)
+    results = []
+    for link in search_results:
+        if (
+            re.search('facebook\.com\/', link) and
+            not re.search('\/posts\/', link)
+        ):
+            results.append((link, 'facebook'))
+        if re.search('\.(nl|nu|com)\/?$', link):
+            results.append((link, 'website'))
+    return results
+
+
 def main():
     results = import_results()
     parties = import_parties()
-    for result in results:
-        if re.sub('\s*\(.+\)$', '', result['Partij'].lower()) not in parties:
-            print(result)
+
+    all_results = []
+    local_parties = get_local_parties_results(results, parties)
+    for lp in local_parties:
+        lp['Sites'] = {y: x for x, y in get_local_party_links(lp)}
+        all_results.append(lp)
+        sleep(1)
+    print(json.dumps(all_results))
     return 0
 
 if __name__ == '__main__':
