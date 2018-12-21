7c1ec955cb5a3883c12b18f901c18c2ce088d9b2
Breyten Ernsting <bje@dds.nl>
2018-12-21 16:05:01 +0100

    Find blogging engine and feed url if applicable.

--- bin/find_blogs.py
+++ bin/find_blogs.py
@@ -7,6 +7,11 @@ import csv
 from time import sleep
 import json
 import configparser
+from copy import deepcopy
+from urllib.parse import urljoin
+
+import requests
+from bs4 import BeautifulSoup
 
 
 def load_config():
@@ -22,12 +27,42 @@ def load_party_info(config):
     return data
 
 
+def _find_blogging_engine(url, soup):
+    elem = soup.find('meta', {'name': 'generator'})
+    if elem is not None:
+        try:
+            engine = elem['content'].split()[0]
+        except Exception:
+            engine = None
+        return engine.lower()
+
+
+def _find_blogging_feed(url, soup):
+    elem = soup.find('link', {'rel': 'alternate', 'type': "application/rss+xml" })
+    if elem is not None:
+        return urljoin(url, elem['href'])
+
+
+def get_blog_info(party, config):
+    result = deepcopy(party)
+    resp = requests.get(party['Sites']['website'])
+    if resp.status_code >= 200 and resp.status_code < 300:
+        soup = BeautifulSoup(resp.content, 'html.parser')
+        result['engine'] = _find_blogging_engine(resp.url, soup)
+        result['feed'] = _find_blogging_feed(resp.url, soup)
+    return result
+
+
 def main():
     config = load_config()
     parties = load_party_info(config)
+    parties_with_blogs = []
     for party in parties:
         if 'website' in party['Sites']:
-            print(party['Sites']['website'])
+            res = get_blog_info(party, config)
+            if res is not None:
+                parties_with_blogs.append(res)
+    print(json.dumps(parties_with_blogs, indent=2))
     return 0
 
 if __name__ == '__main__':
